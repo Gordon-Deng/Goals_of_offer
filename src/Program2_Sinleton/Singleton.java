4628197c7d2989186b9de1f8892bb5116c6cd1e7
Gordon <gordondeng@foxmail.com>
2018-02-12 15:04:01 +0800

    add Singleton

---
+++ src/Program2_Sinleton/Singleton.java
@@ -0,0 +1,31 @@
+package Program2_Sinleton;
+
+/* 实现Singleton模式
+
+   题目描述：设计一个类，我们只能生成该类的一个实例
+
+ * 这种写法考虑了线程安全，将对singleton的null判断以及new的部分使用synchronized进行加锁。
+ * 同时，对singleton对象使用volatile关键字进行限制，保证其对所有线程的可见性，并且禁止对其进行指令重排序优化。
+ * 在getSingleton()方法中，进行两次null检查。看似多此一举，但实际上却极大提升了并发度，进而提升了性能。
+ * 为什么可以提高并发度呢？在单例中new的情况非常少，绝大多数都是可以并行的读操作。
+ * 因此在加锁前多进行一次null检查就可以减少绝大多数的加锁操作，执行效率提高的目的也就达到了
+ */
+
+
+public class Singleton {
+    private static volatile Singleton singleton = null;
+
+    private Singleton(){}
+
+    public static Singleton getSingleton(){
+
+        if(singleton == null){
+            synchronized (Singleton.class){
+                if(singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
